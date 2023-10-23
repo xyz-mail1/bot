8a3e6e60ed0bf1fc3c5b02a4209afcd4ed829241
xyz-mail1 <sunday.september@protonmail.com>
2023-10-23 06:11:35 +0530

    test

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 const dotenv = require('dotenv');
-
 dotenv.config();
-const { Client, Events, GatewayIntentBits, Partials, Collection } = require('discord.js');
-const chalkAnimation = require('chalk-animation');
+const {
+  Client,
+  Events,
+  GatewayIntentBits,
+  Partials, Collection,
+  WebhookClient: W,
+  EmbedBuilder: E,
+  codeBlock } = require('discord.js'),
+  chalkAnimation = require('chalk-animation'),
+  fs = require("node:fs"),
+  path = require('node:path'),
+  client = new Client({
+    intents: [GatewayIntentBits.AutoModerationConfiguration, GatewayIntentBits.AutoModerationExecution, GatewayIntentBits.DirectMessageReactions, GatewayIntentBits.DirectMessageTyping, GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildEmojisAndStickers, GatewayIntentBits.GuildIntegrations, GatewayIntentBits.GuildInvites, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMessageTyping, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildModeration, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildScheduledEvents, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildWebhooks, GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent],
+    partials: [Partials.Message, Partials.Channel, Partials.GuildMember, Partials.Reaction, Partials.GuildScheduledEvent, Partials.User, Partials.ThreadMember],
+    shards: "auto"
+  })
+token = process.env.token,
+  prefix = process.env.prefix;
 
-const fs = require("node:fs");
-const path = require('node:path');
-
-const client = new Client({
-  intents: [GatewayIntentBits.AutoModerationConfiguration, GatewayIntentBits.AutoModerationExecution, GatewayIntentBits.DirectMessageReactions, GatewayIntentBits.DirectMessageTyping, GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildEmojisAndStickers, GatewayIntentBits.GuildIntegrations, GatewayIntentBits.GuildInvites, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMessageTyping, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildModeration, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildScheduledEvents, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildWebhooks, GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent],
-  partials: [Partials.Message, Partials.Channel, Partials.GuildMember, Partials.Reaction, Partials.GuildScheduledEvent, Partials.User, Partials.ThreadMember],
-  shards: "auto"
-});
-
-const token = process.env.token;
-const prefix = process.env.prefix;
+var PrettyError = require('pretty-error');
+var pe = new PrettyError();
+pe.start();
 
 chalkAnimation.rainbow("Starting...")
 
@@ -25,6 +32,9 @@ client.once(Events.ClientReady, c => {
 client.commands = new Collection();
 client.cooldowns = new Collection();
 client.slash = new Collection();
+// webhooks 
+
+const errorLogs = new W({ url: `${process.env.errorHook}` })
 
 // prefix command handler 
 
@@ -89,7 +99,22 @@ client.on(Events.MessageCreate, message => {
   try {
     command.execute(message, args);
   } catch (error) {
-    console.log(error);
+
+    console.log(pe.render(error));
+    if (error) if (error.length > 950) error = error.slice(0, 950) + '... view console for details';
+    if (error.stack) if (error.stack.length > 950) error.stack = error.stack.slice(0, 950) + '... view console for details';
+    if (!error.stack) return
+    const errorEmbed = new E()
+      .setTitle(`⛔ Prefix command error`)
+      .addFields([{ name: "Error", value: error ? codeBlock(error) : "No error" },
+      { name: "Stack error", value: error.stack ? codeBlock(error.stack) : "No stack error" }]);
+    try {
+      errorLogs.send(embeds: [errorEmbed])
+    } catch {
+      console.log('Error sending unhandled promise rejection to webhook')
+      console.log(error)
+
+    }
   }
 
 });
@@ -127,7 +152,7 @@ client.on(Events.InteractionCreate, async interaction => {
   try {
     await command.run(client, interaction);
   } catch (error) {
-    console.error(error);
+    console.log(pe.render(error));
     if (interaction.replied || interaction.deferred) {
       await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
     } else {
