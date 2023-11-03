ad584248704352057f148fd66ec1463dcd16677c
xyz-mail1 <sunday.september@protonmail.com>
2023-11-03 07:08:46 +0530

    modified:   commands/actions/bite.js 	new file:   commands/actions/fuck.js 	modified:   package-lock.json 	modified:   package.json 	modified:   src/handlers/db.js

--- src/handlers/db.js
+++ src/handlers/db.js
@@ -1,4 +1,4 @@
-const sqlite = require('better-sqlite3');
+const sqlite = require("better-sqlite3");
 const db = new sqlite(`../../main.db`);
 
 // Create a table
@@ -13,23 +13,44 @@ db.exec(`
 `);
 
 module.exports = (client) => {
-  client.incrementCount = async function(variable, sender, target) {
+  client.incrementCount = async function (variable, sender, target) {
     // Check if entry for the users already exists in the db
-    const select = db.prepare(`SELECT * FROM counts WHERE (variable = ? AND sender = ? AND target = ?) OR (variable = ? AND sender = ? AND target = ?)`);
-    const entry = await select.get(variable, sender, target, variable, target, sender);
+    const select = db.prepare(
+      `SELECT * FROM counts WHERE (variable = ? AND sender = ? AND target = ?) OR (variable = ? AND sender = ? AND target = ?)`,
+    );
+    const entry = await select.get(
+      variable,
+      sender,
+      target,
+      variable,
+      target,
+      sender,
+    );
     if (entry) {
-      console.log(entry.count);
-      const updateCount = db.prepare(`UPDATE counts SET count = count + 1 WHERE (variable = ? AND sender = ? AND target = ?) OR (variable = ? AND sender = ? AND target = ?)`);
+      const updateCount = db.prepare(
+        `UPDATE counts SET count = count + 1 WHERE (variable = ? AND sender = ? AND target = ?) OR (variable = ? AND sender = ? AND target = ?)`,
+      );
       updateCount.run(variable, sender, target, variable, target, sender);
     } else {
-      const insert = db.prepare(`INSERT INTO counts (variable, sender, target, count) VALUES (?, ?, ?, ?)`);
+      const insert = db.prepare(
+        `INSERT INTO counts (variable, sender, target, count) VALUES (?, ?, ?, ?)`,
+      );
       insert.run(variable, sender, target, 1);
     }
   };
 
-  client.getCount = async function(variable, sender, target) {
-    const stmt = db.prepare('SELECT count FROM counts WHERE (variable = ? AND sender = ? AND target = ?) OR (variable = ? AND sender = ? AND target = ?)');
-    const result = await stmt.all(variable, sender, target, variable, target, sender);
+  client.getCount = async function (variable, sender, target) {
+    const stmt = db.prepare(
+      "SELECT count FROM counts WHERE (variable = ? AND sender = ? AND target = ?) OR (variable = ? AND sender = ? AND target = ?)",
+    );
+    const result = await stmt.all(
+      variable,
+      sender,
+      target,
+      variable,
+      target,
+      sender,
+    );
 
     return result.reduce((total, row) => total + row.count, 1);
   };
