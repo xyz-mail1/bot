e06c368d38f54f789fce22a51e2174c6c246f2e6
xyz-mail1 <sunday.september@protonmail.com>
2023-11-10 06:33:18 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -1,75 +1,17 @@
-const dotenv = require("dotenv");
-dotenv.config();
-const {
-    Client,
-    GatewayIntentBits: GB,
-    Partials,
-    Collection,
-    WebhookClient: W,
-    EmbedBuilder: E,
-    codeBlock,
-  } = require("discord.js"),
-  // chalkAnimation = require("chalk-animation"),
-  fs = require("node:fs"),
-  path = require("node:path"),
-  /*client = new Client({
-    intents: [
-      GB.AutoModerationConfiguration,
-      GB.AutoModerationExecution,
-      GB.DirectMessageReactions,
-      GB.DirectMessageTyping,
-      GB.DirectMessages,
-      GB.GuildEmojisAndStickers,
-      GB.GuildIntegrations,
-      GB.GuildInvites,
-      GB.GuildMembers,
-      GB.GuildMessageReactions,
-      GB.GuildMessageTyping,
-      GB.GuildMessages,
-      GB.GuildModeration,
-      GB.GuildPresences,
-      GB.GuildScheduledEvents,
-      GB.GuildVoiceStates,
-      GB.GuildWebhooks,
-      GB.Guilds,
-      GB.MessageContent,
-    ],
-    partials: [
-      Partials.Message,
-      Partials.Channel,
-      Partials.GuildMember,
-      Partials.Reaction,
-      Partials.GuildScheduledEvent,
-      Partials.User,
-      Partials.ThreadMember,
-    ],
-    shards: "auto",
-  }),*/
-  token = process.env.token;
+require("dotenv").config();
 
-const BotClient = require("./src/structures/bot");
+const { WebhookClient: W, EmbedBuilder: E } = require("discord.js"),
+  token = process.env.token,
+  BotClient = require("./src/structures/bot"),
+  client = new BotClient();
 
-const client = new BotClient();
 client.loadHandlers();
 
 var PrettyError = require("pretty-error");
-var pe = new PrettyError();
-/*
-console.clear();
-chalkAnimation.rainbow("Starting...");*/
-/*
-client.commands = new Collection();
-client.cooldowns = new Collection();
-client.slash = new Collection();
-*/
+const pe = new PrettyError();
+pe.start();
 const errorLogs = new W({ url: `${process.env.errorHook}` });
-/*
-const handlers = fs.readdirSync("./src/handlers/");
 
-for (const file of handlers) {
-  require(`./src/handlers/${file}`)(client);
-}
-*/
 process.on("unhandledRejection", (error) => {
   const errorMessage =
     error.message.length > 950
