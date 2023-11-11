f38c7a8e8e7b25e4065ee7fe52584492e48a7c92
xyz-mail1 <sunday.september@protonmail.com>
2023-11-11 06:45:09 +0530

    modified:   src/commands/actions/bite.js

--- src/commands/actions/bite.js
+++ src/commands/actions/bite.js
@@ -9,29 +9,30 @@ module.exports = {
     const sender = message.author.id;
     const mention = message.mentions.users.first() || message.author;
     const target = mention.id;
-    if (target) {
-      const gif = await api.sfw("bite");
 
-      client.incrementCount("bites", sender, target);
-      const count = await client.getCount("bites", sender, target);
+    const gif = await api.sfw("bite");
+    if (!gif) return message.reply("error getting gif, aborting");
 
-      const embed = new Discord.EmbedBuilder()
-        .setColor("#ffb3b3")
-        .setTitle("You gave a bite!")
-        .setURL("https://discord.com/invite/NQpTcs6r8z")
-        .setDescription(`${message.author} bites ${mention}`)
-        .setImage(gif.link);
-      if (count === 1) {
-        embed.setFooter({ text: `It's their first bite from you!` });
-      } else {
-        embed.setFooter({ text: `That's a total of ${count} bites now!` });
-      }
-      await message.reply({
-        embeds: [embed],
-        allowedMentions: { repliedUser: false },
-      });
+    client.incrementCount("bites", sender, target);
+
+    const count = await client.getCount("bites", sender, target);
+
+    const embed = new Discord.EmbedBuilder()
+      .setColor("#ffb3b3")
+      .setTitle("You gave a bite!")
+      .setURL("https://discord.com/invite/NQpTcs6r8z")
+      .setDescription(`${message.author} bites ${mention}`)
+      .setImage(gif.link);
+
+    if (count === 1) {
+      embed.setFooter({ text: `It's their first bite from you!` });
     } else {
-      message.reply("You need to mention someone to kiss!");
+      embed.setFooter({ text: `That's a total of ${count} bites now!` });
     }
+
+    await message.reply({
+      embeds: [embed],
+      allowedMentions: { repliedUser: false },
+    });
   },
 };
